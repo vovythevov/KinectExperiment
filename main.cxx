388b8f95e0826c604041e66dfebdeb59b98f7d56
Johan Andruejol <johan.andruejol@kitware.com>
2013-04-08 21:22:07 -0400

    First commit -> Compilation Error (Erk)

---
+++ main.cxx
@@ -0,0 +1,11 @@
+ #include <NuiApi.h>
+ #include <windows.h>
+ 
+ #include <iostream>
+ 
+ int main (int argc, char* argv)
+ {
+  std::cout<<"I'm alive, aliiiive !"<<std::endl;
+ 
+  return EXIT_SUCCESS;
+ }
