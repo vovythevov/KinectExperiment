7728134d08645c5e77a3713395ca31696a61c6e3
Johan Andruejol <johan.andruejol@kitware.com>
2013-04-09 18:43:09 -0400

    Using Event !
    The result is much faster and should allow to have multiple streams

--- main.cxx
+++ main.cxx
@@ -20,8 +20,15 @@
 #include <vtkSmartPointer.h>
 
 //-----------------------------------------------------------------------------
+// STATIC Variables
+
+// Picture size
+unsigned int width = 640; // resolution could be deduced from kinect frame
+unsigned int height = 480;
+
 // Kinect variables
 HANDLE rgbStream;              // The identifier of the Kinect's RGB Camera
+//HANDLE depthStream;            // The identifier of the Kinect's depth Camera
 INuiSensor* sensor;            // The kinect sensor
 
 //-----------------------------------------------------------------------------
@@ -44,7 +51,7 @@ bool InitKinect() // source: http://www.cs.princeton.edu/~edwardz/tutorials/kine
 }
 
 //-----------------------------------------------------------------------------
-bool OpenStream()
+bool OpenColorStream(HANDLE readyEvent)
 {
   if (!sensor)
     {
@@ -57,40 +64,48 @@ bool OpenStream()
     NUI_IMAGE_RESOLUTION_640x480, // resolution
     NUI_IMAGE_DEPTH_NO_VALUE, // looks like this controls how far you want to see
     2, // The doc says "Most application should use 2"
-    NULL,  // Next frame event. Maybe I could grab this ???
+    readyEvent,  // Next frame event. Maybe I could grab this ???
     &rgbStream) == EXIT_SUCCESS;
 }
 
 //-----------------------------------------------------------------------------
-vtkImageData* GrabNextFrame(int numberOfTry)
+bool GrabNextFrame(HANDLE handle, int numberOfTry, NUI_IMAGE_FRAME& kinectFrame)
 {
   if (!sensor)
     {
-    return 0;
+    return false;
     }
 
   // Grab frame
-  NUI_IMAGE_FRAME kinectFrame;
   HRESULT result = E_NUI_FRAME_NO_DATA;
   while (numberOfTry >= 0 && result != EXIT_SUCCESS)
     {
-    HRESULT result = sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &kinectFrame);
+    HRESULT result = sensor->NuiImageStreamGetNextFrame(handle, 0, &kinectFrame);
     if (result == EXIT_FAILURE)
       {
       std::cout<<"Could not grab frame ! Error "<<result<<std::endl;
-      return 0;
+      return false;
       }
 
     --numberOfTry;
     Sleep(1);
     }
 
+  return true;
+}
+
+//-----------------------------------------------------------------------------
+vtkImageData* GrabNextColorFrame(int numberOfTry)
+{
+  NUI_IMAGE_FRAME kinectFrame;
+  if (! GrabNextFrame(rgbStream, numberOfTry, kinectFrame))
+    {
+    return 0;
+    }
+
   INuiFrameTexture* texture = kinectFrame.pFrameTexture;
   NUI_LOCKED_RECT lockedRect;
   texture->LockRect(0, &lockedRect, NULL, 0);
-
-  unsigned int width = 640; // resolution could be deduced from kinect frame
-  unsigned int height = 480;
 
   /*std::cout<<"Image Dimension: "
     <<"Number of bytes in a row: "<<lockedRect.Pitch<<std::endl
@@ -113,7 +128,7 @@ vtkImageData* GrabNextFrame(int numberOfTry)
         ++kinectImage;
         }
 
-      kinectImage += VTK_SIZEOF_CHAR;
+      ++kinectImage;
       }
     }
 
@@ -123,56 +138,59 @@ vtkImageData* GrabNextFrame(int numberOfTry)
   return image;
 }
 
-
 //-----------------------------------------------------------------------------
  int main (int argc, char* argv)
  {
-  std::cout<<"I'm alive, aliiiive ! "<<S_OK<<std::endl;
- 
   // 1- init
-  std::cout<<"Is there anybody out there ?"<<std::endl;
-  if (InitKinect())
+  if (!InitKinect())
     {
-    std::cout<<"Yeah ! Found a kinect ! How are you doing ?"<<std::endl;
-    std::cout<<sensor->NuiStatus()<<std::endl;
-    }
-  else
-    {
-    std::cout<<"Well, I guess I'd better go :("<<std::endl;
+    std::cout<<"Could not Init Kinect"<<std::endl;
     return EXIT_FAILURE;
     }
 
-  // 2 - Open stream
-  if (OpenStream())
+  // 2- Create Events
+  HANDLE nextColorFrameEvent = CreateEventW(NULL, TRUE, FALSE, NULL);
+
+  // 2 - Open RGB stream
+  if (!OpenColorStream(nextColorFrameEvent))
     {
-    std::cout<<"Getting the kinect ready"<<std::endl;
-    std::cout<<sensor->NuiStatus()<<std::endl;
-    }
-  else
-    {
-    std::cout<<"NOooooooooooooo :("<<std::endl;
+    std::cout<<"Could not Init Color Stream"<<std::endl;
     return EXIT_FAILURE;
     }
-
 
   // -- VTK Rendering --
-  vtkSmartPointer<vtkImageMapper> imageMapper = vtkSmartPointer<vtkImageMapper>::New();
-  imageMapper->SetColorWindow(255);
-  imageMapper->SetColorLevel(127.5);
 
-  vtkSmartPointer<vtkActor2D> imageActor = vtkSmartPointer<vtkActor2D>::New();
-  imageActor->SetMapper(imageMapper);
+  // 1- Color
+  vtkSmartPointer<vtkImageMapper> colorImageMapper =
+    vtkSmartPointer<vtkImageMapper>::New();
+  colorImageMapper->SetColorWindow(255);
+  colorImageMapper->SetColorLevel(127.5);
+
+  vtkSmartPointer<vtkActor2D> colorImageActor = vtkSmartPointer<vtkActor2D>::New();
+  colorImageActor->SetMapper(colorImageMapper);
+  colorImageActor->SetPosition(0,0);
+
+  // 2 -Depth
+  //vtkSmartPointer<vtkImageMapper> depthImageMapper =
+  //  vtkSmartPointer<vtkImageMapper>::New();
+
+  //vtkSmartPointer<vtkActor2D> depthImageActor = vtkSmartPointer<vtkActor2D>::New();
+  //depthImageActor->SetMapper(depthImageMapper);
+  //depthImageActor->SetPosition(width,0);
 
   // Setup renderers
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+  vtkSmartPointer<vtkRenderer> renderer =
+    vtkSmartPointer<vtkRenderer>::New();
 
   // Setup render window
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+  vtkSmartPointer<vtkRenderWindow> renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  renderWindow->SetSize(640, 480);
+  renderWindow->SetSize(width * 2, height);
 
   //renderer->AddViewProp(imageActor);
-  renderer->AddActor2D(imageActor);
+  renderer->AddActor2D(colorImageActor);
+  //renderer->AddActor2D(depthImageActor);
 
   vtkNew<vtkImageFlip> flip;
   flip->SetFilteredAxis(1);
@@ -180,11 +198,16 @@ vtkImageData* GrabNextFrame(int numberOfTry)
   bool capture = true;
   while (capture)
     {
-    flip->SetInputData(GrabNextFrame(100));
-    flip->Update();
-    imageMapper->SetInputData(flip->GetOutput());
+    if (WAIT_OBJECT_0 == WaitForSingleObject(nextColorFrameEvent, 1))
+      {
+      flip->SetInputData(GrabNextColorFrame(5));
+      flip->Update();
+      colorImageMapper->SetInputData(flip->GetOutput());
 
-    renderWindow->Render();
+      renderWindow->Render();
+      }
+
+    Sleep(1);
     }
 
   return EXIT_SUCCESS;
