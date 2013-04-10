7924efaf0a84c2e8c801201045a2a429392cdd1e
Johan Andruejol <johan.andruejol@kitware.com>
2013-04-09 23:21:00 -0400

    Getting an armautre wohoooooooooo

--- main.cxx
+++ main.cxx
@@ -8,6 +8,7 @@
 
 // -- VTK --
 #include <vtkActor2D.h>
+#include <vtkDataObject.h>
 #include <vtkImageData.h>
 #include <vtkImageFlip.h>
 #include <vtkImageImport.h>
@@ -17,7 +18,18 @@
 #include <vtkNew.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
 #include <vtkSmartPointer.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkPolyDataReader.h>
+#include <vtkProperty.h>
+#include <vtkPolyDataWriter.h>
+
+// -- Bender --
+#include "vtkArmatureWidget.h"
+#include "vtkBoneWidget.h"
+#include "vtkDoubleConeBoneRepresentation.h"
 
 // Places from where I glanced/used/copy some code from:
 // http://www.cs.princeton.edu/~edwardz/tutorials/kinect/kinect1.html
@@ -102,12 +114,12 @@ bool OpenDepthStream(HANDLE readyEvent)
 //-----------------------------------------------------------------------------
 bool OpenSkeletonStream(HANDLE readyEvent)
 {
-  if (!sensor)
+  if (!sensor || !HasSkeletalEngine(sensor))
     {
     return false;
     }
 
-  return sensor->NuiSkeletonTrackingEnable(readyEvent, 0) != EXIT_SUCCESS;
+  return sensor->NuiSkeletonTrackingEnable(readyEvent, 0) == EXIT_SUCCESS;
 }
 
 //-----------------------------------------------------------------------------
@@ -256,6 +268,255 @@ vtkImageData* GrabNextDepthFrame()
 }
 
 //-----------------------------------------------------------------------------
+vtkPolyData* GrabNextSkeletonFrame(vtkArmatureWidget* armature)
+{
+  NUI_SKELETON_FRAME SkeletonFrame;
+  if (NuiSkeletonGetNextFrame(0, &SkeletonFrame) != EXIT_SUCCESS)
+    {
+    return 0;
+    }
+
+  bool foundSkeleton = false;
+  for (int i = 0; i < NUI_SKELETON_COUNT; ++i)
+    {
+    if (SkeletonFrame.SkeletonData[i].eTrackingState == NUI_SKELETON_TRACKED)
+      {
+       foundSkeleton = true;
+      }
+    }
+
+  if (! foundSkeleton)
+    {
+      std::cout<<"No Skeleton Found !"<<std::endl;
+    return 0;
+    }
+
+  NuiTransformSmooth(&SkeletonFrame, NULL);
+
+  int displayPos[20][2];
+  for (int i = 0; i < NUI_SKELETON_COUNT; ++i)
+    {
+    if (SkeletonFrame.SkeletonData[i].eTrackingState != NUI_SKELETON_TRACKED)
+      {
+      continue;
+      }
+
+    for (int j = 0; j < NUI_SKELETON_POSITION_COUNT; ++j)
+      {
+      float fx, fy;
+      NuiTransformSkeletonToDepthImage(
+              SkeletonFrame.SkeletonData[i].SkeletonPositions[j], &fx, &fy);
+      displayPos[j][0] = (int) (fx * SkeletonWidth + 0.5f);
+      displayPos[j][1] = (int) (fy * SkeletonHeight + 0.5f);
+      }
+
+    armature->GetBoneByName("Root")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HIP_CENTER][0],
+      displayPos[NUI_SKELETON_POSITION_HIP_CENTER][1],
+      0);
+
+    // Left leg
+    armature->GetBoneByName("HipLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HIP_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_HIP_LEFT][1],
+      0);
+
+    armature->GetBoneByName("KneeLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_KNEE_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_KNEE_LEFT][1],
+      0);
+
+    armature->GetBoneByName("AnkleLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_ANKLE_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_ANKLE_LEFT][1],
+      0);
+
+    armature->GetBoneByName("FootLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_FOOT_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_FOOT_LEFT][1],
+      0);
+
+    // Right leg
+    armature->GetBoneByName("HipRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HIP_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_HIP_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("KneeRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_KNEE_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_KNEE_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("AnkleRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_ANKLE_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_ANKLE_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("FootRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_FOOT_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_FOOT_RIGHT][1],
+      0);
+
+    // Spine
+    armature->GetBoneByName("Spine")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_SPINE][0],
+      displayPos[NUI_SKELETON_POSITION_SPINE][1],
+      0);
+
+    armature->GetBoneByName("ShoulderCenter")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_CENTER][0],
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_CENTER][1],
+      0);
+
+    armature->GetBoneByName("Head")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HEAD][0],
+      displayPos[NUI_SKELETON_POSITION_HEAD][1],
+      0);
+
+    // Left Arm
+    armature->GetBoneByName("ShoulderLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_LEFT][1],
+      0);
+
+    armature->GetBoneByName("ElbowLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_ELBOW_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_ELBOW_LEFT][1],
+      0);
+
+    armature->GetBoneByName("WristLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_WRIST_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_WRIST_LEFT][1],
+      0);
+
+    armature->GetBoneByName("HandLeft")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HAND_LEFT][0],
+      displayPos[NUI_SKELETON_POSITION_HAND_LEFT][1],
+      0);
+
+    // Right Arm
+    armature->GetBoneByName("ShoulderRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_SHOULDER_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("ElbowRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_ELBOW_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_ELBOW_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("WristRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_WRIST_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_WRIST_RIGHT][1],
+      0);
+
+    armature->GetBoneByName("HandRight")->SetWorldTailRest(
+      displayPos[NUI_SKELETON_POSITION_HAND_RIGHT][0],
+      displayPos[NUI_SKELETON_POSITION_HAND_RIGHT][1],
+      0);
+
+    break;
+    }
+
+
+  return armature->GetPolyData();
+}
+
+//-----------------------------------------------------------------------------
+vtkArmatureWidget* CreateKinectArmature()
+{
+  vtkArmatureWidget* armature = vtkArmatureWidget::New();
+
+  vtkBoneWidget* Root = armature->CreateBone(NULL, "Root");
+  armature->AddBone(Root, NULL);
+  Root->SetWorldHeadRest(0.0, 0.0, 0.0);
+  Root->SetWorldTailRest(0.0, 0.0, 1.0);
+
+  // Left leg
+  vtkBoneWidget* HipLeft = armature->CreateBone(Root, "HipLeft");
+  armature->AddBone(HipLeft, Root);
+  HipLeft->SetWorldTailRest(1.0, 0.0, 1.0);
+
+  vtkBoneWidget* KneeLeft = armature->CreateBone(HipLeft, "KneeLeft");
+  armature->AddBone(KneeLeft, HipLeft);
+  KneeLeft->SetWorldTailRest(1.0, 0.0, -2.0);
+
+  vtkBoneWidget* AnkleLeft = armature->CreateBone(KneeLeft, "AnkleLeft");
+  armature->AddBone(AnkleLeft, KneeLeft);
+  AnkleLeft->SetWorldTailRest(1.0, 0.0, -4.0);
+
+  vtkBoneWidget* FootLeft = armature->CreateBone(AnkleLeft, "FootLeft");
+  armature->AddBone(FootLeft, AnkleLeft);
+  FootLeft->SetWorldTailRest(1.0, 0.0, -5.0);
+
+  // Right leg
+  vtkBoneWidget* HipRight = armature->CreateBone(Root, "HipRight");
+  armature->AddBone(HipRight, Root);
+  HipRight->SetWorldTailRest(-1.0, 0.0, 1.0);
+
+  vtkBoneWidget* KneeRight = armature->CreateBone(HipRight, "KneeRight");
+  armature->AddBone(KneeRight, HipRight);
+  KneeRight->SetWorldTailRest(-1.0, 0.0, -2.0);
+
+  vtkBoneWidget* AnkleRight = armature->CreateBone(KneeRight, "AnkleRight");
+  armature->AddBone(AnkleRight, KneeRight);
+  AnkleRight->SetWorldTailRest(-1.0, 0.0, -4.0);
+
+  vtkBoneWidget* FootRight = armature->CreateBone(AnkleRight, "FootRight");
+  armature->AddBone(FootRight, AnkleRight);
+  FootRight->SetWorldTailRest(-1.0, 0.0, -5.0);
+
+  // spine
+  vtkBoneWidget* Spine = armature->CreateBone(Root, "Spine");
+  armature->AddBone(Spine, Root);
+  Spine->SetWorldTailRest(0.0, 0.0, 2.0);
+
+  vtkBoneWidget* ShoulderCenter = armature->CreateBone(Spine, "ShoulderCenter");
+  armature->AddBone(ShoulderCenter, Spine);
+  ShoulderCenter->SetWorldTailRest(0.0, 0.0, 6.0);
+
+  vtkBoneWidget* Head = armature->CreateBone(ShoulderCenter, "Head");
+  armature->AddBone(Head, ShoulderCenter);
+  Head->SetWorldTailRest(0.0, 0.0, 7.0);
+
+  // Left Arm
+  vtkBoneWidget* ShoulderLeft = armature->CreateBone(ShoulderCenter, "ShoulderLeft");
+  armature->AddBone(ShoulderLeft, ShoulderCenter);
+  ShoulderLeft->SetWorldTailRest(1.0, 0.0, 6.0);
+
+  vtkBoneWidget* ElbowLeft = armature->CreateBone(ShoulderLeft, "ElbowLeft");
+  armature->AddBone(ElbowLeft, ShoulderLeft);
+  ElbowLeft->SetWorldTailRest(1.0, 0.0, 4.0);
+
+  vtkBoneWidget* WristLeft = armature->CreateBone(ElbowLeft, "WristLeft");
+  armature->AddBone(WristLeft, ElbowLeft);
+  WristLeft->SetWorldTailRest(1.0, 0.0, 2.0);
+
+  vtkBoneWidget* HandLeft = armature->CreateBone(WristLeft, "HandLeft");
+  armature->AddBone(HandLeft, WristLeft);
+  HandLeft->SetWorldTailRest(1.0, 0.0, 1.0);
+
+  // Left Arm
+  vtkBoneWidget* ShoulderRight = armature->CreateBone(ShoulderCenter, "ShoulderRight");
+  armature->AddBone(ShoulderRight, ShoulderCenter);
+  ShoulderRight->SetWorldTailRest(-1.0, 0.0, 6.0);
+
+  vtkBoneWidget* ElbowRight = armature->CreateBone(ShoulderRight, "ElbowRight");
+  armature->AddBone(ElbowRight, ShoulderRight);
+  ElbowRight->SetWorldTailRest(-1.0, 0.0, 4.0);
+
+  vtkBoneWidget* WristRight = armature->CreateBone(ElbowRight, "WristRight");
+  armature->AddBone(WristRight, ElbowRight);
+  WristRight->SetWorldTailRest(-1.0, 0.0, 2.0);
+
+  vtkBoneWidget* HandRight = armature->CreateBone(WristRight, "HandRight");
+  armature->AddBone(HandRight, WristRight);
+  HandRight->SetWorldTailRest(-1.0, 0.0, 1.0);
+
+  return armature;
+}
+
+//-----------------------------------------------------------------------------
  int main (int argc, char* argv)
  {
   // 1- init
@@ -289,10 +550,11 @@ vtkImageData* GrabNextDepthFrame()
 
   if (!OpenSkeletonStream(nextSkeletonFrameEvent))
     {
-    std::cout<<"Could not Init skeleton Stream"<<std::endl;
+    std::cout<<"Could not Init skeleton Stream "<< HasSkeletalEngine(sensor) <<std::endl;
     return EXIT_FAILURE;
     }
 
+  vtkSmartPointer<vtkArmatureWidget> armature = CreateKinectArmature();
 
   // -- VTK Rendering --
 
@@ -324,11 +586,6 @@ vtkImageData* GrabNextDepthFrame()
   skeletonImageMapper->SetColorWindow(255);
   skeletonImageMapper->SetColorLevel(127.5);
 
-  vtkSmartPointer<vtkActor2D> skeletonImageActor =
-    vtkSmartPointer<vtkActor2D>::New();
-  skeletonImageActor->SetMapper(depthImageMapper);
-  skeletonImageActor->SetPosition(SkeletonWidth, 0);
-
   // Setup renderers
   vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
@@ -337,28 +594,44 @@ vtkImageData* GrabNextDepthFrame()
   vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
-  renderWindow->SetSize(ColorWidth + DepthWidth + SkeletonWidth, ColorHeight);
+  /*renderWindow->SetSize(ColorWidth + DepthWidth + SkeletonWidth, ColorHeight);
 
   renderer->AddActor2D(colorImageActor);
   renderer->AddActor2D(depthImageActor);
-  renderer->AddActor2D(skeletonImageActor);
 
   vtkNew<vtkImageFlip> flipColor;
   flipColor->SetFilteredAxis(1);
 
   vtkNew<vtkImageFlip> flipDepth;
-  flipDepth->SetFilteredAxis(1);
+  flipDepth->SetFilteredAxis(1);*/
+
+
+  // -- Armature Rendering --
+  vtkNew<vtkPolyDataWriter> w;
+  w->SetFileName("W:/Bender/Data/A.vtk");
+
+  vtkSmartPointer<vtkPolyDataMapper> armatureMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+
+  vtkSmartPointer<vtkActor> armatureActor =
+    vtkSmartPointer<vtkActor>::New();
+  armatureActor->SetMapper(armatureMapper);
+  armatureActor->GetProperty()->SetColor(1.0, 0.0, 0.0); 
+  armatureActor->SetOrigin(ColorWidth + DepthWidth + SkeletonWidth /2 , SkeletonHeight / 2, 0);
+
+  // -- Event Loop --
 
   bool capture = true;
   while (capture)
     {
-    if ( WAIT_OBJECT_0 == WaitForSingleObject(nextColorFrameEvent, 0) )
+    /*if ( WAIT_OBJECT_0 == WaitForSingleObject(nextColorFrameEvent, 0) )
       {
       flipColor->SetInputData(GrabNextColorFrame());
       flipColor->Update();
       colorImageMapper->SetInputData(flipColor->GetOutput());
 
       renderWindow->Render();
+      //armatureRenderWindow->Render();
       }
 
     if ( WAIT_OBJECT_0 == WaitForSingleObject(nextDepthFrameEvent, 0) )
@@ -368,16 +641,19 @@ vtkImageData* GrabNextDepthFrame()
       depthImageMapper->SetInputData(flipDepth->GetOutput());
 
       renderWindow->Render();
-      }
+      //armatureRenderWindow->Render();
+      }*/
 
-    if ( WAIT_OBJECT_0 == WaitForSingleObject(nextDepthFrameEvent, 0) )
+    if ( WAIT_OBJECT_0 == WaitForSingleObject(nextSkeletonFrameEvent, 0) )
       {
-      flipDepth->SetInputData(GrabNextDepthFrame());
-      flipDepth->Update();
-      depthImageMapper->SetInputData(flipDepth->GetOutput());
+      std::cout<<"Grabbing armature"<<std::endl;
+      w->SetInputData(GrabNextSkeletonFrame(armature));
+      w->Write();
 
-      renderWindow->Render();
+      std::cout<<"Wrote !"<<std::endl;
       }
+
+    Sleep(500);
 
     }
 
